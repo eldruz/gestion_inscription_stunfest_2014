2549efbe6834ad877da4376ba8c29a34dca9b679
Andrés Saraos Luna <eldruz@outlook.com>
2014-05-01 14:43:42 +0200

    configuration pour serveur de deploiement

--- stunfest_2014/wsgi.py
+++ stunfest_2014/wsgi.py
@@ -8,6 +8,10 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
+import sys
+sys.path.append('/home/eldruz/Projects/stunfest_2014/stunfest_2014')
+sys.path.append('/home/eldruz/Projects/stunfest_2014')
+sys.path.append('/home/eldruz/Projects/stunfest_2014/inscription')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "stunfest_2014.settings")
 
 from django.core.wsgi import get_wsgi_application
