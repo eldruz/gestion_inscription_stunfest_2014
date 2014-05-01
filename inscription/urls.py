5858076be69b542dc298ec83209b014158b43256
Andrés Saraos Luna <eldruz@outlook.com>
2014-05-01 14:56:01 +0200

    mis un nom a toutes les urls pour facilité d'accès

--- inscription/urls.py
+++ inscription/urls.py
@@ -5,8 +5,8 @@ from inscription.models import Joueur, Tournoi, Participant
 # URLS pour le gestionnaire d'inscription
 
 urlpatterns = patterns('',
-        url(r'^liste_joueurs/$', 'inscription.views.joueur_table_view', name='liste_joueurs'),
-        url(r'^inscription/$', 'inscription.views.fiche_inscription'),
+        url(r'^liste_joueurs/$', 'inscription.views.joueur_table_view', name='joueurs'),
+        url(r'^inscription/$', 'inscription.views.fiche_inscription', name='inscription'),
         url(r'^profil/(?P<pseudo_joueur>.*?)/$','inscription.views.profil_joueur',name='profils'),
         url(r'^tournoi/(?P<tournoi_titre>.*)/$', 'inscription.views.joueurs_inscrits_liste', name='tournois'),
         )
